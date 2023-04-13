a0d4e22b58790fcd293b4c7f5fa86335c99f8504
sad-ele <72252304+sad-ele@users.noreply.github.com>
2023-04-13 14:06:29 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,8 +6,5 @@ from scipy import stats
 chat_id = 170380561 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    stat, p_value = stats.ks_2samp(x, y)
-    return p_value < 0.02 # Ваш ответ, True или False
+    return (stats.anderson_ksamp([x, y]).significance_level < 0.02)
+
